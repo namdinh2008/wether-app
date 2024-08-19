eeff885a979575358b08edf4d3a26166fb376b75
namdinh2008 <derricrane@gmail.com>
2024-08-19 09:12:35 +0700

    Initial commit

---
+++ src/components/api.js
@@ -0,0 +1,11 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '30e6e3e3c2mshe6a15acd3887b43p1f6260jsn386d0e8e70e0',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/";
+export const WEATHER_API_KEY = "dbf95d5932095d5fc1787d788eef5a54"
